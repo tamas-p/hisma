525ffc603aa7b7e687e509f00acaafbe176fa6a3
Tamas Palagyi <tamas.palagyi@gmail.com>
2024-11-18 19:18:09 +0100

    refactor: Using Checker instead of function calls

--- examples/hisma_flutter_test/test/impl/t03_hierarchy_test.dart
+++ examples/hisma_flutter_test/test/impl/t03_hierarchy_test.dart
@@ -1,30 +1,35 @@
+import 'package:flutter/material.dart' as m;
 import 'package:flutter_test/flutter_test.dart';
+import 'package:hisma/hisma.dart';
 import 'package:hisma_flutter/hisma_flutter.dart';
 import 'package:hisma_flutter_test/simple_machine.dart';
 import 'package:hisma_flutter_test/t03_hierarchical.dart';
+import 'package:hisma_visual_monitor/hisma_visual_monitor.dart';
 
-import '../../test/aux/aux.dart';
+// import '../../test/aux/aux.dart';
+
+enum Act { fire, tap, back }
 
 void main() {
-  // StateMachine.monitorCreators = [
-  //   (m) => VisualMonitor(m, host: '192.168.122.1'),
-  // ];
+  StateMachine.monitorCreators = [
+    (m) => VisualMonitor(m, host: '192.168.122.1'),
+  ];
   // auxInitLogging();
   testWidgets(
     'overlay test with direct fire',
     (tester) async {
-      await checkAllStates(tester, act: Act.fire);
+      await testAllStates(tester, act: Act.fire);
     },
   );
   testWidgets(
     'overlay test with taps',
     (tester) async {
-      await checkAllStates(tester, act: Act.tap);
+      await testAllStates(tester, act: Act.tap);
     },
   );
 }
 
-Future<void> checkAllStates(
+Future<void> testAllStates(
   WidgetTester tester, {
   required Act act,
 }) async {
@@ -34,98 +39,149 @@ Future<void> checkAllStates(
   await tester.pumpWidget(app);
   expect(machine.activeStateId, machine.initialStateId);
   checkTitle(machine);
-  await checkMachine(
+
+  final checker = Checker(
     tester: tester,
     act: act,
     machine: machine,
-    m: app.generator.mapping,
+    mapping: app.generator.mapping,
   );
+
+  await checker.checkMachine();
 }
 
-Future<void> checkMachine({
-  required WidgetTester tester,
-  required Act act,
-  required StateMachineWithChangeNotifier<S, E, T> machine,
-  required Map<S, Presentation> m,
-}) async {
-  const cm = checkMachine;
-  for (var i = 0; i < machine.states.length; i++) {
-    await check(machine, tester, E.self, mapping: m, cm: cm, act: act);
-    await check(machine, tester, E.forward, mapping: m, cm: cm, act: act);
+class Checker {
+  Checker({
+    required this.tester,
+    required this.act,
+    required this.machine,
+    required this.mapping,
+  });
 
-    final presentation = m[machine.activeStateId];
-    final overlay = presentation is PageCreator && presentation.overlay;
-    if (overlay) {
-      await check(machine, tester, E.back, mapping: m, cm: cm, act: Act.back);
-    } else {
-      await check(machine, tester, E.back, mapping: m, cm: cm, act: act);
+  final WidgetTester tester;
+  final Act act;
+  final StateMachineWithChangeNotifier<S, E, T> machine;
+  final Map<S, Presentation> mapping;
+
+  Future<void> checkMachine() async {
+    for (var i = 0; i < machine.states.length; i++) {
+      await check(E.self, act: act);
+      await check(E.forward, act: act);
+
+      final presentation = mapping[machine.activeStateId];
+      final overlay = presentation is PageCreator && presentation.overlay;
+      if (overlay) {
+        await check(E.back, act: Act.back);
+      } else {
+        await check(E.back, act: act);
+      }
+
+      await check(E.self, act: act);
+
+      // presentation = mapping[machine.activeStateId];
+      // overlay = presentation is PageCreator && presentation.overlay;
+      // if (overlay) {
+      // await _check(machine, tester, E.back, mapping, act: Act.back);
+      // } else {
+      await check(E.back, act: act);
+      // }
+
+      await check(E.self, act: act);
+      await check(E.forward, act: act);
+      await check(E.self, act: act);
+      await check(E.forward, act: act);
+    }
+  }
+
+  Future<void> check(
+    E event, {
+    Act act = Act.tap,
+  }) async {
+    S whereTo(S s, E e) {
+      final state = machine.states[s] as State<E, T, S>?;
+      final a = state!.etm[e];
+      final t = a![0];
+      final transition = machine.transitions[t] as Transition<S>?;
+      return transition!.to;
     }
 
-    await check(machine, tester, E.self, mapping: m, cm: cm, act: act);
+    final s = machine.activeStateId;
+    if (s == null) throw Exception('Machine ${machine.name} is not started.');
+    final expected = whereTo(s, event);
 
-    // presentation = mapping[machine.activeStateId];
-    // overlay = presentation is PageCreator && presentation.overlay;
-    // if (overlay) {
-    // await _check(machine, tester, E.back, mapping, act: Act.back);
-    // } else {
-    await check(machine, tester, E.back, mapping: m, cm: cm, act: act);
-    // }
+    await action(machine, tester, event, act: act);
+    expect(machine.activeStateId, expected);
 
-    await check(machine, tester, E.self, mapping: m, cm: cm, act: act);
-    await check(machine, tester, E.forward, mapping: m, cm: cm, act: act);
-    await check(machine, tester, E.self, mapping: m, cm: cm, act: act);
-    await check(machine, tester, E.forward, mapping: m, cm: cm, act: act);
-  }
-}
-
-/*
-Future<void> _check(
-  StateMachineWithChangeNotifier<S, E, T> machine,
-  WidgetTester tester,
-  E event,
-  Map<S, Presentation> mapping, {
-  Act act = Act.tap,
-}) async {
-  S whereTo(S s, E e) {
-    final state = machine.states[s] as State<E, T, S>?;
-    final a = state!.etm[e];
-    final t = a![0];
-    final transition = machine.transitions[t] as Transition<S>?;
-    return transition!.to;
-  }
-
-  final s = machine.activeStateId;
-  if (s == null) throw Exception('Machine ${machine.name} is not started.');
-  final expected = whereTo(s, event);
-
-  await action(machine, tester, event, act: act);
-  expect(machine.activeStateId, expected);
-
-  // Check if new mapping is a Router
-  final presentation = mapping[machine.activeStateId];
-  if (presentation is PageCreator && presentation.widget is w.Builder) {
     final childMachine = _getChildMachine(machine);
     if (childMachine != null) {
-      await checkMachine(
+      final childChecker = Checker(
         tester: tester,
         act: act,
         machine: childMachine,
-        mapping: childMachine.routerDelegate.mapping,
+        mapping: mapping,
       );
+
+      await childChecker.checkMachine();
+    } else {
+      checkTitle(machine);
     }
-  } else {
-    checkTitle(machine);
   }
 }
 
-StateMachineWithChangeNotifier<S, E, T>? _getChildMachine(
+StateMachineWithChangeNotifier<S, E, T>? _getChildMachine<S, E, T>(
   StateMachineWithChangeNotifier<S, E, T> machine,
 ) {
   final state = machine.states[machine.activeStateId];
-  if (state is State<E, T, S>) {
+  if (state is State<E, T, S> && state.regions.isNotEmpty) {
     return state.regions[0].machine as StateMachineWithChangeNotifier<S, E, T>;
   } else {
     return null;
   }
 }
-*/
+
+Future<void> action<S, E, T>(
+  StateMachineWithChangeNotifier<S, E, T> machine,
+  WidgetTester tester,
+  E? event, {
+  Act act = Act.tap,
+}) async {
+  if (act == Act.fire && event != null) {
+    await machine.fire(event);
+    // We need this extra pumpAndSettle as pageless routes are created in a
+    // subsequent frame by Future.delayed.
+    // TODO: Remove this as new design will not use Future.delayed.
+    await tester.pumpAndSettle();
+  } else if (act == Act.tap && event != null) {
+    await tester.tap(find.text(getButtonTitle(machine, event)).first);
+  } else if (act == Act.back) {
+    final backButton = find.byType(m.BackButton);
+    await tester.tap(backButton);
+  } else {
+    throw Exception('Unsupported trigger: $act');
+  }
+  await tester.pumpAndSettle();
+}
+
+void checkTitle<S, E, T>(StateMachine<S, E, T> machine, [S? stateId]) {
+  // TODO: Use [] representation of hierarchic states.
+  // expect(machine.activeStateId, stateId);
+
+  // final activeMachines = getActiveMachines(machine);
+  // final lm = activeMachines.last;
+  // final path = getTitle(lm, lm.activeStateId);
+  final path = getTitle(machine, machine.activeStateId);
+
+  expect(find.text(path), findsOneWidget);
+}
+
+String getTitle(
+  StateMachine<dynamic, dynamic, dynamic> machine,
+  dynamic stateId,
+) =>
+    '${machine.name} - $stateId';
+
+String getButtonTitle<S, E, T>(
+  StateMachine<S, E, T> machine,
+  dynamic event,
+) =>
+    '${machine.name}.$event';
