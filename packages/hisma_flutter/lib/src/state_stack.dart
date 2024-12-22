af0894e143bc630f51b1e71f7037945dbda51ef5
Tamas Palagyi <tamas.palagyi@gmail.com>
2024-12-22 16:21:25 +0100

    chore: removed old commented out code from StateStack

--- packages/hisma_flutter/lib/src/state_stack.dart
+++ packages/hisma_flutter/lib/src/state_stack.dart
@@ -5,96 +5,6 @@ import 'package:flutter/foundation.dart';
 import 'assistance.dart';
 import 'creator.dart';
 
-/*
-class StateStackO<S> {
-  StateStackO(this.mapping);
-  final Map<S, Presentation> mapping;
-
-  final List<S> _stateIds = [];
-
-  // TODO: Can we avoid exposing the list?
-  // List<S> get list => _stateIds;
-
-  void clear() {
-    _stateIds.clear();
-    print('CLEAR: $_stateIds');
-  }
-
-  void add(S stateId) {
-    _stateIds.add(stateId);
-    print('ADD($stateId): $_stateIds');
-  }
-
-  void remove(S stateId) {
-    _stateIds.remove(stateId);
-    print('REMOVE($stateId): $_stateIds');
-  }
-
-  void removeByStr(String? stateIdStr) {
-    _stateIds.removeWhere((element) => element.toString() == stateIdStr);
-    print('REMOVE_BY_STR($stateIdStr): $_stateIds');
-  }
-  // void removeList(List<S> stateIds) =>
-  //     _stateIds.removeWhere((element) => stateIds.contains(element));
-
-  void cleanUpCircle(S activeStateId) {
-    _stateIds.removeRange(
-      _stateIds.indexOf(activeStateId) + 1,
-      _stateIds.length,
-    );
-    print('CLEANUP_CIRCLE($activeStateId): $_stateIds');
-  }
-
-  bool contains(S? stateId) => _stateIds.contains(stateId);
-
-  void goThrough(void Function(S stateId) processor) {
-    for (final current in _stateIds) {
-      processor(current);
-    }
-  }
-
-  void windBack(S target, void Function(S stateId) processor) {
-    assert(contains(target));
-    final cpy = List<S>.from(_stateIds); // Avoid concurrent mod. exception.
-    for (final current in cpy.reversed) {
-      if (current == target) break;
-      processor(current);
-    }
-  }
-
-  /// Gives back whether jumping to the given stateId will pass PageCreators.
-  bool intermediatePageCreator(S stateId) {
-    if (contains(stateId)) {
-      for (final s in _stateIds.reversed) {
-        if (s == stateId) break;
-        if (mapping[s] is PageCreator) return true;
-      }
-    }
-    return false;
-  }
-
-  bool isNext(S stateId, bool Function(Presentation?) next) {
-    final i = _stateIds.indexOf(stateId);
-    if (_stateIds.length > i + 1) {
-      final nextStateId = _stateIds[i + 1];
-      if (next(mapping[nextStateId])) return true;
-    }
-    return false;
-  }
-
-  bool hasImperatives(S stateId) =>
-      isNext(stateId, (presentation) => presentation is ImperativeCreator);
-
-  bool rightBeforePage(S stateId) =>
-      isNext(stateId, (presentation) => presentation is PageCreator);
-
-  bool isLast(S stateId) {
-    return _stateIds.indexOf(stateId) == _stateIds.length - 1;
-  }
-}
-*/
-//------------------------------------------------------------------------------
-
 class StateStack {
   StateStack();
 
@@ -133,8 +43,6 @@ class StateStack {
     _stack.removeWhere((element, p) => element == key);
     printIt('REMOVE_BY_STR($key)');
   }
-  // void removeList(List<S> stateIds) =>
-  //     _stateIds.removeWhere((element) => stateIds.contains(element));
 
   void cleanUpCircle(String key) {
     final keys = _stack.keys.toList();
@@ -180,17 +88,6 @@ class StateStack {
     }
   }
 
-  /// Gives back whether jumping to the given stateId will pass PageCreators.
-  // bool intermediatePageCreator(S stateId) {
-  //   if (contains(stateId)) {
-  //     for (final s in _stateIds.reversed) {
-  //       if (s == stateId) break;
-  //       if (mapping[s] is PageCreator) return true;
-  //     }
-  //   }
-  //   return false;
-  // }
-
   bool hasImperatives(String key) {
     final keys = _stack.keys.toList();
     for (var i = keys.indexOf(key); i < keys.length; i++) {
@@ -198,9 +95,6 @@ class StateStack {
     }
     return false;
   }
-
-  // bool rightBeforePage(S stateId) =>
-  //     isNext(stateId, (presentation) => presentation is PageCreator);
 
   bool isLast(String key) {
     return _stack.keys.toList().indexOf(key) == _stack.length - 1;
