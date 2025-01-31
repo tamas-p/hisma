70a675175af3c02155252a087d1c8495c8866b7e
Tamas Palagyi <tamas.palagyi@gmail.com>
2025-01-31 08:57:56 +0100

    test: addition tests for history testing

--- packages/hisma/test/history_test.dart
+++ packages/hisma/test/history_test.dart
@@ -8,86 +8,75 @@ void main() {
     test('No history', () async {
       final checker =
           Checker(machine: smNoHistory(), ml1: nhl1, ml2: nhl2, ml3: nhl3);
-
-      await checker.startAndTurnOnAll();
-      checker(l1: S.on, l2: S.on, l3: S.on);
-
-      await checker.turnL1Off();
-      checker(l1: S.off, l2: null, l3: null);
-
-      await checker.turnL1On();
-      checker(l1: S.on, l2: S.off, l3: null);
+      await checker(first: E.on, s: E.on, l1: S.on, l2: S.off, l3: null);
     });
     test('Shallow history', () async {
       final checker =
           Checker(machine: smShallow(), ml1: shl1, ml2: shl2, ml3: shl3);
-
-      await checker.startAndTurnOnAll();
-      checker(l1: S.on, l2: S.on, l3: S.on);
-
-      await checker.turnL1Off();
-      checker(l1: S.off, l2: null, l3: null);
-
-      await checker.turnL1On();
-      checker(l1: S.on, l2: S.on, l3: S.off);
+      await checker(first: E.on, s: E.on, l1: S.on, l2: S.on, l3: S.off);
     });
     test('Deep history', () async {
       final checker =
           Checker(machine: smDeep(), ml1: dhl1, ml2: dhl2, ml3: dhl3);
-
-      await checker.startAndTurnOnAll();
-      checker(l1: S.on, l2: S.on, l3: S.on);
-
-      await checker.turnL1Off();
-      checker(l1: S.off, l2: null, l3: null);
-
-      await checker.turnL1On();
-      checker(l1: S.on, l2: S.on, l3: S.on);
+      await checker(first: E.on, s: E.on, l1: S.on, l2: S.on, l3: S.on);
     });
 
     group('History endpoints', () {
-      test('No HistoryEndpoint used', () async {
-        final checker =
-            Checker(machine: smHistoryEPs(), ml1: hel1, ml2: hel2, ml3: hel3);
-
-        await checker.startAndTurnOnAll();
-        checker(l1: S.on, l2: S.on, l3: S.on);
-
-        await checker.turnL1Off();
-        checker(l1: S.off, l2: null, l3: null);
-
-        await checker.turnL1On();
-        checker(l1: S.on, l2: S.off, l3: null);
+      group('with E.on', () {
+        test('E.on to turn on, No HistoryEndpoint used', () async {
+          final checker =
+              Checker(machine: smHistoryEPs(), ml1: hel1, ml2: hel2, ml3: hel3);
+          await checker(first: E.on, s: E.on, l1: S.on, l2: S.off, l3: null);
+        });
+        test('E.shallow to turn on, No HistoryEndpoint used', () async {
+          final checker =
+              Checker(machine: smHistoryEPs(), ml1: hel1, ml2: hel2, ml3: hel3);
+          await checker(
+              first: E.shallow, s: E.on, l1: S.on, l2: S.off, l3: null);
+        });
+        test('E.deep to turn on, No HistoryEndpoint used', () async {
+          final checker =
+              Checker(machine: smHistoryEPs(), ml1: hel1, ml2: hel2, ml3: hel3);
+          await checker(first: E.deep, s: E.on, l1: S.on, l2: S.off, l3: null);
+        });
       });
-      test('Shallow HistoryEndpoint used', () async {
-        final checker =
-            Checker(machine: smHistoryEPs(), ml1: hel1, ml2: hel2, ml3: hel3);
-
-        await checker.startAndTurnOnAll();
-        checker(l1: S.on, l2: S.on, l3: S.on);
-
-        await checker.turnL1Off();
-        checker(l1: S.off, l2: null, l3: null);
-
-        await checker.turnL1Shallow();
-        checker(l1: S.on, l2: S.on, l3: S.off);
+      group('with E.shallow', () {
+        test('E.on to turn on, Shallow HistoryEndpoint used', () async {
+          final checker =
+              Checker(machine: smHistoryEPs(), ml1: hel1, ml2: hel2, ml3: hel3);
+          await checker(
+              first: E.on, s: E.shallow, l1: S.on, l2: S.on, l3: S.off);
+        });
+        test('E.shallow to turn on, Shallow HistoryEndpoint used', () async {
+          final checker =
+              Checker(machine: smHistoryEPs(), ml1: hel1, ml2: hel2, ml3: hel3);
+          await checker(
+              first: E.shallow, s: E.shallow, l1: S.on, l2: S.on, l3: S.off);
+        });
+        test('E.deep to turn on, Shallow HistoryEndpoint used', () async {
+          final checker =
+              Checker(machine: smHistoryEPs(), ml1: hel1, ml2: hel2, ml3: hel3);
+          await checker(
+              first: E.deep, s: E.shallow, l1: S.on, l2: S.on, l3: S.off);
+        });
       });
-      test('Deep HistoryEndpoint used', () async {
-        final checker = Checker(
-          machine: smHistoryEPs(),
-          ml1: hel1,
-          ml2: hel2,
-          ml3: hel3,
-        );
-
-        await checker.startAndTurnOnAll();
-        checker(l1: S.on, l2: S.on, l3: S.on);
-
-        await checker.turnL1Off();
-        checker(l1: S.off, l2: null, l3: null);
-
-        await checker.turnL1Deep();
-        checker(l1: S.on, l2: S.on, l3: S.on);
+      group('with E.deep', () {
+        test('E.onDeep to turn on, HistoryEndpoint used', () async {
+          final checker =
+              Checker(machine: smHistoryEPs(), ml1: hel1, ml2: hel2, ml3: hel3);
+          await checker(first: E.on, s: E.deep, l1: S.on, l2: S.on, l3: S.on);
+        });
+        test('E.shallow to turn on, Deep HistoryEndpoint used', () async {
+          final checker =
+              Checker(machine: smHistoryEPs(), ml1: hel1, ml2: hel2, ml3: hel3);
+          await checker(
+              first: E.shallow, s: E.deep, l1: S.on, l2: S.on, l3: S.on);
+        });
+        test('E.deep to turn on, Deep HistoryEndpoint used', () async {
+          final checker =
+              Checker(machine: smHistoryEPs(), ml1: hel1, ml2: hel2, ml3: hel3);
+          await checker(first: E.deep, s: E.deep, l1: S.on, l2: S.on, l3: S.on);
+        });
       });
     });
   });
@@ -106,30 +95,31 @@ class Checker {
   final String ml2;
   final String ml3;
 
-  Future<void> turnL1Off() async {
-    await machine.fire(E.off);
-  }
-
-  Future<void> turnL1On() async {
-    await machine.fire(E.on);
-  }
-
-  Future<void> turnL1Shallow() async {
-    await machine.fire(E.shallow);
-  }
-
-  Future<void> turnL1Deep() async {
-    await machine.fire(E.deep);
-  }
-
-  Future<void> startAndTurnOnAll() async {
+  Future<void> startAndTurnOnAll(E event) async {
     await machine.start();
-    await machine.find<S, E, T>(ml1).fire(E.on);
-    await machine.find<S, E, T>(ml2).fire(E.on);
+    await machine.find<S, E, T>(ml1).fire(event);
+    await machine.find<S, E, T>(ml2).fire(event);
+    // For l3 we do not have shallow and deep events so we use 'on' always.
     await machine.find<S, E, T>(ml3).fire(E.on);
   }
 
-  void call({required S? l1, required S? l2, required S? l3}) {
+  Future<void> call({
+    required E first,
+    required E s,
+    required S? l1,
+    required S? l2,
+    required S? l3,
+  }) async {
+    await startAndTurnOnAll(first);
+    checkResult(l1: S.on, l2: S.on, l3: S.on);
+    await machine.fire(E.off);
+    checkResult(l1: S.off, l2: null, l3: null);
+
+    await machine.fire(s);
+    checkResult(l1: l1, l2: l2, l3: l3);
+  }
+
+  void checkResult({required S? l1, required S? l2, required S? l3}) {
     expect(machine.find<S, E, T>(ml1).activeStateId, equals(l1));
     expect(machine.find<S, E, T>(ml2).activeStateId, equals(l2));
     expect(machine.find<S, E, T>(ml3).activeStateId, equals(l3));
