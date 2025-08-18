e2cf51e67e863ad2d74009ae1d2e38cfdd725c51
Tamas Palagyi <tamas.palagyi@gmail.com>
2025-08-18 22:27:52 +0200

    fix: Using fvm exec for melos

--- release.sh
+++ release.sh
@@ -28,15 +28,15 @@ fi
 cecho "Next steps are going to analyze, test and pana all packages."
 proceed
 
-melos analyze
+fvm exec melos analyze
 echeck "melos analyze failed. Check logs."
 
 message "Check  all packages by melos test:"
-melos test
+fvm exec melos test
 echeck "melos test failed. Check logs."
 
 message "Check all packages by melos pana:"
-melos run --no-select pana
+fvm exec melos run --no-select pana
 echeck "melos pana failed. Check logs."
 
 separator
@@ -44,7 +44,7 @@ cecho "All checks are completed successfully."
 cecho "Next step is versioning the packages."
 proceed
 BEFORE_TAGS=$(git tag)
-melos version
+fvm exec melos version
 
 if [ "$(git tag)" != "$BEFORE_TAGS" ]; then
     git push --follow-tags
@@ -57,12 +57,12 @@ fi
 separator
 cecho "Next step is to test publishing (--dry-run)."
 proceed
-melos publish --dry-run
+fvm exec melos publish --dry-run
 
 separator
 cecho "Next step is REAL publishing to pub.dev."
 proceed
-melos publish --no-dry-run
+fvm exec melos publish --no-dry-run
 
 
 separator
